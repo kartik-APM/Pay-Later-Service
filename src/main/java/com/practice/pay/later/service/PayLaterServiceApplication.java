e8a548693bd88aa8c7177a425f0840a8d763d3fc
PaNdA-KaRTiK <kartikpanda663@gmail.com>
2021-07-13 09:59:11 +0530

    Project Initialization

---
+++ src/main/java/com/practice/pay/later/service/PayLaterServiceApplication.java
@@ -0,0 +1,13 @@
+package com.practice.pay.later.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayLaterServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayLaterServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practice/pay/later/service/PayLaterServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practice.pay.later.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayLaterServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
